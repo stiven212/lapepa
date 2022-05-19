890067bc92af971df9f995e4e52016cb0d44d3b7
stiven212 <stivfunk2000@hotmail.com>
2022-05-19 10:35:46 -0500

    modificaciones en operaciones

---
+++ operaciones.js
@@ -0,0 +1,14 @@
+slc = () => {
+
+
+//    alert("formulario trabajando")
+
+    numero1 = document.getElementById("fnumber").value;
+    numero2 = document.getElementById("snumber").value;
+
+    
+    alert(numero1+ numero2)
+
+
+
+}
