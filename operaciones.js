19cd383bcad2d39e09a8b9d849b7f357f0469a92
Estefy24jn <joiznikolalde24.jm@gmail.com>
2022-05-19 10:39:54 -0500

    funcion dividir

---
+++ operaciones.js
@@ -0,0 +1,14 @@
+dividir=()=>{
+    let num1= parseInt(document.getElementById("numero1"));
+    
+   let num2= parseInt(document.getElementById("numero2"));
+
+   
+
+   let dividir= num1/num2;
+
+   alert("El resultado es: "+dividir);
+
+
+}
+
