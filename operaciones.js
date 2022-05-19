7eb6a75863f770f292ddee109f9db84df58b1409
AntonioVillegas13 <antonio.villegas@epn.edu.ec>
2022-05-19 10:50:06 -0500

    Se agrego el archivo html Av y se añadio la funcion mayor en el archivo operaciones.js

--- operaciones.js
+++ operaciones.js
@@ -1,14 +1,25 @@
 slc = () => {
+  //    alert("formulario trabajando")
+
+  numero1 = document.getElementById("fnumber").value;
+  numero2 = document.getElementById("snumber").value;
+numer1= parseFloat(numero1)
+numer2= parseFloat(numero2)
 
 
-//    alert("formulario trabajando")
+  alert(numero1 + numero2);
 
-    numero1 = document.getElementById("fnumber").value;
-    numero2 = document.getElementById("snumber").value;
-
-    
-    alert(numero1+ numero2)
+};
 
 
+mayor = () => {
+  let dato1 = document.getElementById("dato1").value;
+  let dato2 = document.getElementById("dato2").value;
 
-}
+  console.log(dato1 ,dato2);
+  if (dato1 > dato2) {
+    alert( "el mayor es: "+dato1 )
+  } else if (dato2 > dato1) {
+    alert( "el mayor es: "+dato2)
+  }
+};
