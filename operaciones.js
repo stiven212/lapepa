79695696ad4cf2c5b9ab35074a23da18ab1feab4
Jeanpy171 <jeanpyfuentes67@gmail.com>
2022-05-19 11:16:27 -0500

    Implementacion de multiplicacion

---
+++ operaciones.js
@@ -0,0 +1,10 @@
+
+
+
+
+ multiplicar = () => {
+    let number1 = parseFloat(document.getElementById("numero-1").value)
+    let number2 = parseFloat(document.getElementById("numero-2").value)
+
+    alert("Resultado: "+(number1 * number2))
+}
